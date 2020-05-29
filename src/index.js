9f0be33ad491071c8bd2ce7a8076bb82654598d5
Edgar Nikoghosyan <edgarnikogosjan@gmail.com>
2020-05-30 02:07:51 +0400

    add useReducer example

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css'; 
 // import App from "./App" 
-import App from "./Components/React_HOOKS/useCallback/App"
+import App from "./Components/React_HOOKS/useReducer/App"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
