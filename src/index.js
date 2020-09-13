2060f110a7f8253b5f77546760f751112ea96127
BrownEdgar <nikogosjanedgar@gmail.com>
2020-09-13 17:49:05 +0400

    merge request

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css'; 
 // import App from "./App" 
-import App from "./Components/HTTP/App"
+import App from "./Components/SITE/copy/App"
+import data from './Components/SITE/data';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	//<React.StrictMode>
-	<App/>,
+	<App data={data}/>,
 	//</React.StrictMode>,
 	document.getElementById('root'));
 
