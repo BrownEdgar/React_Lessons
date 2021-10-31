ed32af97b4034022758188a746802b8f233da8f3
BrownEdgar <nikogosjanedgar@gmail.com>
2021-10-31 17:19:36 +0400

    set a Default |  basics start project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+function App() {
+	return <Button variant="contained">Hello World</Button>;
+}
+
+export default App
+
