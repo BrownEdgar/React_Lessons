4255a592723b9c1c7256e6e661ef726732bd05b1
BrownEdgar <nkogosjanedgar@gmail.com>
2022-01-05 15:59:06 +0400

    add UseForm basics examples

---
+++ src/Components/UseForm/App.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import Headers from "./Header";
+import "./style.css"
+export default function App() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => alert(JSON.stringify(data));
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Headers />
+
+                <input {...register('first name', { required: true })}  />
+
+                <input {...register('last name', { required: true })} />
+
+                <input type="submit" />
+            </form>
+        </div>
+
+    );
+}
