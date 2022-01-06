05eaf4c4c9f1f68c6d7a7c7e870959621f0d04be
BrownEdgar <nikogosjanedgar@gmail.com>
2022-01-06 23:46:36 +0400

    add more Test example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/Test/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router} from 'react-router-dom'
 
