a10fe03e1dbd3bbf6e9f5276cfddd191f7bb328b
BrownEdgar <nikogosjanedgar@gmail.com>
2022-12-12 12:57:57 +0400

    update ReadMe file, change lesson 1 ex

---
+++ Lesson_1/test.js
@@ -0,0 +1,31 @@
+
+
+
+
+const myName = 985623
+
+function App() {
+	return <div>
+		<h1>{myName}</h1>
+	</div>
+}
+
+
+
+let container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
